c4f564d2f3d535b4fdfd4fd662a7fd8b9703bb24
doneill <doneill@esri.com>
2015-03-07 15:36:57 -0900

    added floating action button

--- 2015-DS/MaterialBaseMaps/app/src/main/java/com/esri/runtime/android/materialbasemaps/ui/MainActivity.java
+++ 2015-DS/MaterialBaseMaps/app/src/main/java/com/esri/runtime/android/materialbasemaps/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.esri.runtime.android.materialbasemaps.ui;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
