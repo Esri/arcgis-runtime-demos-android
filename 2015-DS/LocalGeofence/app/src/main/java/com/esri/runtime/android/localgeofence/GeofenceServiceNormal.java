cf65b505492c65dabe794f54d005366ed511bf29
Shelly Gill <sgill@esri.com>
2015-03-10 22:29:15 -0700

    Initial commit of local geofence demo for Developer Summit 2015 Building Great Android App Uis and UXs session.

---
+++ 2015-DS/LocalGeofence/app/src/main/java/com/esri/runtime/android/localgeofence/GeofenceServiceNormal.java
@@ -0,0 +1,247 @@
+package com.esri.runtime.android.localgeofence;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.esri.core.geometry.Point;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.Random;
+
+/**
+ * An IntentService to receive updates from the Google Fusion API at a less
+ * frequent rate of updates. Also responsible for unsubscribing from updates
+ * and starting the fast update service instead when the device moves close to
+ * or inside the geofence.
+ */
+public class GeofenceServiceNormal extends IntentService {
+
+  private static final String TAG = GeofenceServiceNormal.class.getSimpleName();
+
+  GoogleApiClient mGoogleApiClient = null;
+
+  public GeofenceServiceNormal() {
+    super(TAG);
+  }
+
+  @Override
+  protected void onHandleIntent(Intent intent) {
+    if (intent != null) {
+      final String action = intent.getAction();
+
+      if (MainActivity.ACTION_CHECK_LOCATION.equals(action)) {
+        final Location location = intent.getParcelableExtra(
+            FusedLocationProviderApi.KEY_LOCATION_CHANGED);
+        handleActionCheckLocation(location);
+      }
+      else if (MainActivity.ACTION_START_NORMAL_UPDATES.equals(action)) {
+          handleActionStartNormalUpdates();
+      }
+      else if (MainActivity.ACTION_START_FAST_UPDATES.equals(action)) {
+        handleActionChangeToFastUpdates();
+      }
+    }
+  }
+
+  /**
+   * Start receiving location updates, by connecting to the Google API client,
+   * and registering to receive location updates at a less frequent rate,
+   * using balanced power and accuracy location settings.
+   * If this is called, then we assume that the Fast updates service is not
+   * running already.
+   */
+  private void handleActionStartNormalUpdates() {
+
+    // Connect to the Google API client.
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addApi(LocationServices.API)
+        .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+
+          @Override
+          public void onConnected(Bundle bundle) {
+
+            // Define settings for Google Fusion API location updates. Use balanced power
+            // and accuracy, and ensure updates are not requested too frequently, but
+            // allow updates to be sent when other apps request data updates.
+            LocationRequest balancedRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setFastestInterval(MainActivity.NORMAL_FASTEST_UPDATE_INTERVAL)
+                .setInterval(MainActivity.NORMAL_UPDATE_INTERVAL)
+                .setSmallestDisplacement(MainActivity.NORMAL_MIN_DISPLACEMENT);
+
+            // Create a PendingIntent for this service, and send this to the
+            // requestLocationUpdates method.
+            Intent serviceIntent = new Intent(MainActivity.ACTION_CHECK_LOCATION,
+                null, getApplicationContext(), GeofenceServiceNormal.class);
+
+            PendingIntent pendingIntent = PendingIntent.getService(
+                GeofenceServiceNormal.this, 0, serviceIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+            // Start the request for location updates.
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, balancedRequest, pendingIntent);
+
+            // Disconnect the client, not required until next change.
+            mGoogleApiClient.disconnect();
+            mGoogleApiClient = null;
+          }
+
+          @Override
+          public void onConnectionSuspended(int i) {
+
+          }
+        })
+        .build();
+
+    mGoogleApiClient.connect();
+  }
+
+  /**
+   * Stop this service, and register the other normal update service to receive
+   * location updates instead.
+   */
+  private void handleActionChangeToFastUpdates() {
+    // Connect to the Google API client.
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addApi(LocationServices.API)
+        .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+
+          @Override
+          public void onConnected(Bundle bundle) {
+            Intent selfIntent = new Intent(MainActivity.ACTION_CHECK_LOCATION,
+                null, getApplicationContext(), GeofenceServiceNormal.class);
+            PendingIntent selfPendingIntent = PendingIntent.getService(
+                getApplicationContext(), 0, selfIntent, 0);
+            final PendingResult<Status> cancelFastResult =
+                LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, selfPendingIntent);
+            cancelFastResult.setResultCallback(new ResultCallback<Status>() {
+              @Override
+              public void onResult(Status status) {
+                Log.i(TAG, "handleActionChangeToFastUpdates PendingResult Status:" + status);
+              }
+            });
+
+            LocationRequest accurateRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setFastestInterval(MainActivity.FAST_FASTEST_UPDATE_INTERVAL)
+                .setInterval(MainActivity.FAST_UPDATE_INTERVAL)
+                .setSmallestDisplacement(MainActivity.FAST_MIN_DISPLACEMENT);
+
+            // Create a PendingIntent to start the GeofenceServiceFast.
+            Intent serviceIntent = new Intent(MainActivity.ACTION_CHECK_LOCATION,
+                null, getApplicationContext(), GeofenceServiceFast.class);
+
+            PendingIntent pendingIntent = PendingIntent.getService(
+                getApplicationContext(), 0, serviceIntent, 0);
+
+            // Register the PendingIntent to receive location updates.
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, accurateRequest, pendingIntent);
+
+            // Disconnect the client, not required until next change.
+            mGoogleApiClient.disconnect();
+            mGoogleApiClient = null;
+          }
+
+          @Override
+          public void onConnectionSuspended(int i) {
+
+          }
+        })
+        .build();
+
+    mGoogleApiClient.connect();
+  }
+
+
+  /**
+   * This action is handled in the provided background thread. It is called
+   * when this service receives a location update from Google fusion API;
+   * therefore no connection to the Google API client is required.
+   */
+  private void handleActionCheckLocation(Location newLocation) {
+
+    if(newLocation !=null) {
+      // The incoming location is the current device location, in geographic coordinates.
+      Point locationPoint = new Point(newLocation.getLongitude(), newLocation.getLatitude());
+      LocalGeofence.FenceInformation info = LocalGeofence.latestLocation(locationPoint);
+      Log.i(TAG, String.format("GeofenceServiceNormal Status: %s, UpdateChange: %s, Change: %s", info.status, info.updateChange, info.change));
+
+      if (info.change == LocalGeofence.Change.ENTERED) {
+        sendNotification(String.format("Alert! Entered %s", LocalGeofence.getFeatureName()));
+      }
+      else if (info.change == LocalGeofence.Change.EXITED) {
+        sendNotification(String.format("Exited %s", LocalGeofence.getFeatureName()));
+      }
+
+      if (LocalGeofence.UpdateChange.FASTER == info.updateChange) {
+        // Ensure we are receiving updates frequently.
+        handleActionChangeToFastUpdates();
+      }
+      else if (LocalGeofence.UpdateChange.SLOWER == info.updateChange) {
+        // Ensure we are receiving updates less frequently.
+        handleActionStartNormalUpdates();
+      }
+    }
+  }
+
+  public void sendNotification(String title) {
+    try {
+      NotificationManager notificationManager = null;
+
+      Context ctx = getApplicationContext();
+      if (ctx != null)
+      {
+        notificationManager = (NotificationManager) ctx.getSystemService(NOTIFICATION_SERVICE);
+      }
+      else {
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+      }
+
+      // Build a notification.
+      Notification.Builder notificationBuilder = new Notification.Builder(this);
+      notificationBuilder.setContentTitle(title);
+      notificationBuilder.setContentText(LocalGeofence.getSubtitle());
+      notificationBuilder.setSmallIcon(R.drawable.ic_fence_simple);
+      Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_geofence_bright);
+      notificationBuilder.setLargeIcon(largeIcon);
+      if (Build.VERSION.SDK_INT >= 21) {
+        // API 21 and over -
+        notificationBuilder.setColor(getResources().getColor(R.color.material_blue_700));
+      }
+
+      // Notification API was introduced at v11, but there were some additional changes from v16.
+      // We only need to deal with changes since v14.
+      Random r = new Random();
+      int id = r.nextInt();
+      if (Build.VERSION.SDK_INT < 16) {
+        notificationManager.notify(id, notificationBuilder.getNotification());
+      } else {
+        notificationManager.notify(id, notificationBuilder.build());
+      }
+    }
+    catch (Exception ex) {
+      Log.i(TAG, ex.getMessage());
+    }
+  }
+
+
+}
---
+++ 2015-DS/LocalGeofence/app/src/main/java/com/esri/runtime/android/localgeofence/MainActivity.java
@@ -0,0 +1,311 @@
+package com.esri.runtime.android.localgeofence;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.esri.core.geodatabase.Geodatabase;
+import com.esri.core.geodatabase.GeodatabaseFeatureTable;
+import com.esri.core.geometry.Polygon;
+import com.esri.core.map.Feature;
+import com.esri.core.table.TableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MainActivity extends Activity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  // Latest Google Play Services must be installed and available on device.
+  //http://www.zionsoft.net/2014/02/google-play-services-set-up/
+  private GoogleApiClient mGoogleClient;
+
+  // Geodatabase and table containing fence features to choose from.
+  public static Geodatabase mGdb = null;
+  public static GeodatabaseFeatureTable mGdbFeatureTable = null;
+
+  // Define two sets of update intervals, for normal updates, and for fast updates
+  // when device is near the fence. GeofenceService will only be assigned power
+  // blame for the interval set by xx_UPDATE_INTERVAL, but can still receive
+  // locations triggered by other applications at a rate up to
+  // xx_FASTEST_UPDATE_INTERVAL. Note that these are set to suit demo purposes,
+  // but should be tailored to the speciifc usage.
+  public static final int NORMAL_UPDATE_INTERVAL = 1800000; // 30 minutes
+  public static final int NORMAL_FASTEST_UPDATE_INTERVAL = 25000; // 25 seconds
+  public static final int NORMAL_MIN_DISPLACEMENT = 150; // Meters
+  public static final int FAST_UPDATE_INTERVAL = 15000; // 15 seconds
+  public static final int FAST_FASTEST_UPDATE_INTERVAL = 5000; // 5 seconds
+  public static final int FAST_MIN_DISPLACEMENT = 25; // Meters
+
+  public static final String ACTION_CHECK_LOCATION = "com.esri.arcgis.android.samples.localgeofence.action.CHECK_LOCATION";
+  public static final String ACTION_START_NORMAL_UPDATES = "com.esri.arcgis.android.samples.localgeofence.action.NORMAL_UPDATES";
+  public static final String ACTION_START_FAST_UPDATES = "com.esri.arcgis.android.samples.localgeofence.action.FAST_UPDATES";
+
+  // Intent Extra IDs
+  public static final String GEOFENCE_FEATURE_OBJECTID_EXTRA_ID = "com.esri.arcgis.android.samples.geometryobjectid";
+
+
+  // Geodatabase of geofences. Set this to any geodatabase, ensuring that the
+  // approproate Layer ID and display field names are also set below.
+  public static final String GEODATABASE_FILEPATH = "/ArcGIS/California/Cali2.geodatabase";
+  public static final int FENCE_LAYER_ID = 1;
+  public static final String FENCE_NAME_FIELD = "NAME";
+  public static final String FENCE_OBJECTID_FIELD = "OBJECTID";
+
+  // Keys for shared preferences.
+  //public static final String KEY_FENCE_SUBTITLE = "fenceSubtitle";
+  //public static final String KEY_FENCE_GEOMETRY_OID = "fenceGeometryOid";
+
+  EditText mFenceSubtitleEditText;
+  TextView mIntroTextView;
+  TextView mFenceNameTextView;
+  Button mChooseFenceButton;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mIntroTextView = (TextView)findViewById(R.id.textViewIntro);
+    mFenceNameTextView = (TextView)findViewById(R.id.textViewFenceName);
+    mFenceSubtitleEditText = (EditText)findViewById(R.id.editTextFenceSubtitle);
+    mChooseFenceButton = (Button) findViewById(R.id.buttonChooseName);
+
+    // Connect to geodatabase
+    boolean haveGdb = setupGeodatabase();
+    if (! haveGdb) {
+      mChooseFenceButton.setEnabled(false);
+      Toast.makeText(MainActivity.this, "Geofence geodatabase not found",
+          Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == R.id.start_service_menu_item) {
+      startGeofenceService();
+      return true;
+    }
+    else if (id == R.id.stop_service_menu_item) {
+      stopServices();
+      return true;
+    }
+    else if (id == R.id.choose_location_map_menu_item) {
+      selectOnMap();
+      return true;
+    }
+    else if (id == R.id.show_location_menu_item) {
+      showCurrentLocation();
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  public void onClick_SelectFromList(View view) {
+    selectFromList();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (mGdb != null) {
+      mGdb.dispose();
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    // Receive results from either map or list geofence selection - both give
+    // exactly the same information.
+    if (data == null) return;
+
+    if (resultCode == RESULT_OK) {
+
+      long fenceOid = data.getLongExtra(GEOFENCE_FEATURE_OBJECTID_EXTRA_ID, -1);
+      Feature fenceFeature = getFeatureFromGeodatabase(fenceOid);
+      if (fenceFeature != null) {
+        Polygon fencePolygon = (Polygon)fenceFeature.getGeometry();
+        String fenceName = fenceFeature.getAttributeValue(FENCE_NAME_FIELD).toString();
+        LocalGeofence.setFence(fencePolygon, mGdbFeatureTable.getSpatialReference());
+        LocalGeofence.setFeatureName(fenceName);
+        LocalGeofence.setFeatureOid(fenceOid);
+
+        mFenceNameTextView.setText(fenceName);
+      }
+    }
+  }
+
+  /**
+   * Send an intent to start the geofence service that listens to location
+   * updates at a normal rate of frequency.
+   */
+  private void startGeofenceService() {
+
+    LocalGeofence.setSubtitle(mFenceSubtitleEditText.getText().toString());
+
+    Intent serviceIntent = new Intent(MainActivity.ACTION_START_NORMAL_UPDATES,
+        null, this, GeofenceServiceNormal.class);
+    PendingIntent pendingIntent = PendingIntent.getService(this, 0,
+        serviceIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    try {
+      pendingIntent.send();
+    }
+    catch (PendingIntent.CanceledException e) {
+      Toast.makeText(MainActivity.this, "Problem starting geofence service",
+          Toast.LENGTH_SHORT).show();
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Unsubscribe both of the geofence services from location updates.
+   */
+  private void stopServices() {
+    mGoogleClient = new GoogleApiClient.Builder(this)
+        .addApi(LocationServices.API)
+        .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+          @Override
+          public void onConnected(Bundle bundle) {
+            Log.i(TAG, "MainActivity.stopServiceByConnectAndRemove.onConnected");
+
+            // Create a PendingIntent for this GeofenceServiceNormal class
+            // and use it to unregister from location updates.
+            Intent fastIntent = new Intent(MainActivity.ACTION_CHECK_LOCATION,
+                null, MainActivity.this, GeofenceServiceFast.class);
+            PendingIntent fastPendingIntent = PendingIntent.getService(
+                MainActivity.this, 0, fastIntent, 0);
+            final PendingResult<Status> cancelFastResult = LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleClient, fastPendingIntent);
+
+            cancelFastResult.setResultCallback(new ResultCallback<Status>() {
+              @Override
+              public void onResult(Status status) {
+                Log.i(TAG, "    removeLocationUpdates PendingResult Status:" + status);
+                Toast.makeText(MainActivity.this, "Stopped Fast Location Updates", Toast.LENGTH_SHORT).show();
+              }
+            });
+
+            Intent normalIntent = new Intent(MainActivity.ACTION_CHECK_LOCATION,
+                null, MainActivity.this, GeofenceServiceNormal.class);
+            PendingIntent normalPendingIntent = PendingIntent.getService(
+                MainActivity.this, 0, normalIntent, 0);
+            final PendingResult<Status> cancelNormalResult = LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleClient, normalPendingIntent);
+            cancelNormalResult.setResultCallback(new ResultCallback<Status>() {
+              @Override
+              public void onResult(Status status) {
+                Log.i(TAG, "    removeLocationUpdates PendingResult Status:" + status);
+                Toast.makeText(MainActivity.this, "Stopped Normal Location Updates", Toast.LENGTH_SHORT).show();
+              }
+            });
+
+            // Disconnect the client again, no longer required unless user
+            // wants to start updates again.
+            mGoogleClient.disconnect();
+            mGoogleClient = null;
+          }
+
+          @Override
+          public void onConnectionSuspended(int i) {
+
+          }
+        })
+        .build();
+
+    mGoogleClient.connect();
+  }
+
+
+
+  /**
+   * Start an activity that allows the user to select a geofence feature from a list.
+   */
+  private void selectFromList() {
+    Intent geofenceListIntent = new Intent(MainActivity.this, GeofenceListActivity.class);
+
+    // If working against multiple layers, use Extras to send layer IDs.
+    startActivityForResult(geofenceListIntent, FENCE_LAYER_ID);
+  }
+
+  /**
+   * Start an activity that allows the user to select a geofence feature on a map.
+   */
+  private void selectOnMap() {
+    Intent geofenceMapIntent = new Intent(MainActivity.this, GeofenceMapActivity.class);
+    // If working against multiple layers, use Extras to send layer IDs.
+    startActivityForResult(geofenceMapIntent, FENCE_LAYER_ID);
+  }
+
+  /**
+   * Start an activity that shows the users current location, so they can track
+   * progress.
+   */
+  private void showCurrentLocation() {
+    Intent currentLocationIntent = new Intent(MainActivity.this,
+        CurrentLocationActivity.class);
+     // No result is required as we dont need any data from this activity.
+    startActivity(currentLocationIntent);
+  }
+
+  /**
+   * Get a feature from the geodatabase by its ObjectID.s
+   */
+  private Feature getFeatureFromGeodatabase(long featureOid) {
+    if (featureOid < 0) {
+      return null;
+    }
+    Feature feature = null;
+    try {
+      feature = mGdbFeatureTable.getFeature(featureOid);
+    } catch (TableException e) {
+      e.printStackTrace();
+    }
+    return feature;
+  }
+
+  /**
+   * Open the geodatabase of geofence features.
+   * @return true if geodatabase was opened, otherwise false.
+   */
+  private boolean setupGeodatabase() {
+    File rootFile = Environment.getExternalStorageDirectory();
+    File dataFile = new File(rootFile, GEODATABASE_FILEPATH);
+    if (! dataFile.exists()) return false;
+
+    String filePath = dataFile.getAbsolutePath();
+    try {
+      mGdb = new Geodatabase(filePath);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return false;
+    }
+
+    if (mGdb == null) return false;
+    mGdbFeatureTable = mGdb.getGeodatabaseFeatureTableByLayerId(FENCE_LAYER_ID);
+    return (mGdbFeatureTable != null);
+  }
+
+}
