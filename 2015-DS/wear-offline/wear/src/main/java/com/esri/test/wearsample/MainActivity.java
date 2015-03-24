4bd91cbfd91bfdf3826f83a2825e168f8b70734b
Puneet Prakash <puneet_prakash@esri.com>
2015-03-24 11:49:12 -0700

    changed the licenses, add info to readme

--- 2015-DS/wear-offline/wear/src/main/java/com/esri/test/wearsample/MainActivity.java
+++ 2015-DS/wear-offline/wear/src/main/java/com/esri/test/wearsample/MainActivity.java
@@ -1,3 +1,23 @@
+/* Copyright 2015 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * A copy of the license is available in the repository's
+ * https://github.com/Esri/arcgis-runtime-demos-android/blob/master/license.txt
+ *
+ * For information about licensing your deployed app, see
+ * https://developers.arcgis.com/android/guide/license-your-app.htm
+ *
+ */
+
 package com.esri.test.wearsample;
 
 import android.app.Activity;
@@ -79,6 +99,10 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+      // Set license level for the demo. Override "license_client_id" and
+      // "license_standard_string" in strings.xml to use Standard license level.
+      // Otherwise comment out the following line to use Developer license level.
+      // @see <a href="https://developers.arcgis.com/android/guide/license-your-app.htm">License your app</a>
       setLicense();
       setContentView(R.layout.activity_main);
 
@@ -124,8 +148,6 @@ public class MainActivity extends Activity {
 //      String path = Environment.getExternalStorageDirectory().getPath() + "/ArcGIS/SoCalBasemap.tpk";
 
   }
-
-
 
 
 
